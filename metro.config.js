30f5da9689a342752a90648283be56590448eeef
Ana Alice <douradoalice88@gmail.com>
2024-05-06 15:18:26 -0300

    feat: metro config and assets

---
+++ metro.config.js
@@ -0,0 +1,24 @@
+const {
+  getDefaultConfig: getDefaultExpoConfig,
+} = require("@expo/metro-config");
+
+metroConfig = (() => {
+  const config = getDefaultExpoConfig(__dirname);
+  config.resolver.sourceExts.push("cjs");
+
+  const { transformer, resolver } = config;
+
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
+
+  return config;
+})();
+
+module.exports = metroConfig;
