d199dc460dc78eafaf55706024b65aa007681182
Ana Alice <douradoalice88@gmail.com>
2024-05-16 13:24:47 -0300

    feat: signIn inputs

--- src/screens/auth/SignIn/index.tsx
+++ src/screens/auth/SignIn/index.tsx
@@ -8,6 +8,7 @@ import { Text, TouchableOpacity } from "react-native";
 import { Spacer } from "../../../components/Spacer";
 
 import IconNormal from "../../../assets/icons/EyeClosed.svg";
+import IconError from "../../../assets/icons/EyeClosedRed.svg";
 
 export const SignIn = () => {
   const formSchema = yup.object({
@@ -15,13 +16,14 @@ export const SignIn = () => {
       .string()
       .required("Campo obrigatório")
       .min(8, "Digite pelo menos 8 dígitos"),
+    email: yup.string().required("Campo obrigatório").email(),
   });
 
   const {
     control,
     handleSubmit,
     formState: { errors },
-  } = useForm<{ name: string }>({
+  } = useForm<{ name: string; email: string }>({
     resolver: yupResolver(formSchema),
   });
 
@@ -32,8 +34,31 @@ export const SignIn = () => {
   return (
     <Container style={{ marginTop: 60 }}>
       <Spacer height={50} />
-
       <Content>
+        <Input.Root>
+          <Input.Label label="Nome" errors={errors?.name!} />
+          <Input.Content errors={errors?.name!}>
+            <Input.TextInput
+              control={control}
+              name="name"
+              placeholder="digite o seu nome"
+            />
+            <Input.Icon icon={<IconNormal />} iconError={<IconError />} />
+          </Input.Content>
+        </Input.Root>
+        <Spacer height={20} />
+        <Input.Root>
+          <Input.Label label="E-mail" errors={errors?.email!} />
+          <Input.Content errors={errors?.email!}>
+            <Input.TextInput
+              control={control}
+              name="email"
+              placeholder="digite seu e-mail"
+            />
+            <Input.Icon icon={<IconNormal />} iconError={<IconError />} />
+          </Input.Content>
+        </Input.Root>
+
         <TouchableOpacity
           style={{
             height: 50,
